b3dce41a4e3caaf2fbbdde73cd3f5517c6dc7b9f
jikkskv <jikkskv@gmail.com>
2021-09-11 11:06:52 +0800

    Initial commit

---
+++ src/main/java/com/carousell/cli/test/App.java
@@ -0,0 +1,13 @@
+package com.carousell.cli.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
